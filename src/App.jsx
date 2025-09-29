1841db76b013be6dcbb6bcc88348e062ebba15f1
Gaber Usef <dev.gaber@gmail.com>
2025-09-29 04:20:11 +0300

    fix lazy bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import {Toaster} from "react-hot-toast";
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import {lazy, Suspense} from "react";
 import useAuthListener from "./features/auth/useAuthListener";
+import DoctorAppLayout from "./features/Dashboard/DoctorAppLayout";
+import ProviderAppLayout from "./features/Dashboard/ProviderAppLayout";
+import ProviderDashboard from "./features/Dashboard/ProviderDashboard";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import ProviderClinics from "./pages/ProviderClinics";
+import ProviderRentals from "./pages/ProviderRentals";
+import {default as ProviderSettings} from "./pages/ProviderSettings";
+import Signup from "./pages/Signup";
 import Home from "./ui/Home/Home";
 import NotFound from "./ui/NotFound";
 import ProtectedRoutes from "./ui/ProtectedRoutes";
 import RoleProtectedRoute from "./ui/RoleProtectedRoute";
-import Spinner from "./ui/Spinner";
-
-// Lazy load all non-critical components
-const ReactQueryDevtools = lazy(() =>
-  import("@tanstack/react-query-devtools").then((module) => ({
-    default: module.ReactQueryDevtools,
-  }))
-);
+import ProviderPayments from "./pages/ProviderPayments";
+import DoctorFindClinics from "./pages/DoctorFindClinics";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,12 +30,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<Spinner />}>
-        <ReactQueryDevtools
-          initialIsOpen={false}
-          buttonPosition="bottom-left"
-        />
-      </Suspense>
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
       <AuthWrapper />
     </QueryClientProvider>
   );
@@ -43,48 +41,46 @@ function AuthWrapper() {
 
   return (
     <BrowserRouter>
-      <Suspense fallback={<Spinner />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-          <Route element={<ProtectedRoutes />}>
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route
-              path="provider"
-              element={
-                <RoleProtectedRoute role="provider">
-                  <ProviderAppLayout />
-                </RoleProtectedRoute>
-              }>
-              <Route index element={<Navigate to="dashboard" />} />
-              <Route path="dashboard" element={<ProviderDashboard />} />
-              <Route path="clinics" element={<ProviderClinics />} />
-              <Route path="rentals" element={<ProviderRentals />} />
-              <Route path="payments" element={<ProviderPayments />} />
-              <Route path="settings" element={<ProviderSettings />} />
-            </Route>
-
-            <Route
-              path="doctor"
-              element={
-                <RoleProtectedRoute role="doctor">
-                  <DoctorAppLayout />
-                </RoleProtectedRoute>
-              }>
-              <Route index element={<Navigate to="clinics" />} />
-              <Route path="clinics" element={<DoctorFindClinics />} />
-              <Route path="rentals" element={<ProviderRentals />} />
-              <Route path="payments" element={<ProviderPayments />} />
-              <Route path="settings" element={<ProviderSettings />} />
-            </Route>
+        <Route element={<ProtectedRoutes />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route
+            path="provider"
+            element={
+              <RoleProtectedRoute role="provider">
+                <ProviderAppLayout />
+              </RoleProtectedRoute>
+            }>
+            <Route index element={<Navigate to="dashboard" />} />
+            <Route path="dashboard" element={<ProviderDashboard />} />
+            <Route path="clinics" element={<ProviderClinics />} />
+            <Route path="rentals" element={<ProviderRentals />} />
+            <Route path="payments" element={<ProviderPayments />} />
+            <Route path="settings" element={<ProviderSettings />} />
           </Route>
 
-          <Route path="signup" element={<Signup />} />
-          <Route path="login" element={<Login />} />
+          <Route
+            path="doctor"
+            element={
+              <RoleProtectedRoute role="doctor">
+                <DoctorAppLayout />
+              </RoleProtectedRoute>
+            }>
+            <Route index element={<Navigate to="clinics" />} />
+            <Route path="clinics" element={<DoctorFindClinics />} />
+            <Route path="rentals" element={<ProviderRentals />} />
+            <Route path="payments" element={<ProviderPayments />} />
+            <Route path="settings" element={<ProviderSettings />} />
+          </Route>
+        </Route>
 
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
 
       <Toaster
         position="top-center"
