0b700f950f9af569a3368399e88a984483a92a8f
alinasr783 <alinasreldin783@gmail.com>
2025-10-19 23:48:14 +0300

    real data

--- src/ui/Header/Logo.jsx
+++ src/ui/Header/Logo.jsx
@@ -5,7 +5,7 @@ function Logo() {
     <Link to="/">
       <span className="inline-block">
         <img
-          src="https://i.ibb.co/r2TtmRWJ/Generated-Image-October-19-2025-10-42-PM.png"
+          src="https://i.ibb.co/TxMzV8JL/Generated-Image-October-19-2025-11-44-PM.png"
           alt="Connect Share logo"
           className="h-12 sm:h-14 md:h-16 inline-block object-contain"
         />
